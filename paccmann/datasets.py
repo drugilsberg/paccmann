4b89bf3cc8d86570f4937e346f931db76a55a728
Jannis Born <jannis.born@gmx.de>
2019-10-16 14:33:32 +0200

    added sklearn to requirements and code polishing

--- paccmann/datasets.py
+++ paccmann/datasets.py
@@ -3,64 +3,31 @@ import os
 import logging
 import tensorflow as tf
 
-
 logger = logging.getLogger('datasets')
 
 # TFRecords metadata.
 DATASETS_METADATA = {
-    'smiles_character_tokens': tf.FixedLenFeature(
-        (158), tf.int64
-    ),
-    'smiles_atom_tokens': tf.FixedLenFeature(
-        (155), tf.int64
-    ),
-    'fingerprints_256': tf.FixedLenFeature(
-        (256), tf.int64
-    ),
-    'fingerprints_512': tf.FixedLenFeature(
-        (512), tf.int64
-    ),
-    'targets_10': tf.FixedLenFeature(
-        ((10, 1)), tf.float32
-    ),
-    'targets_20': tf.FixedLenFeature(
-        ((20, 1)), tf.float32
-    ),
-    'targets_50': tf.FixedLenFeature(
-        ((50, 1)), tf.float32
-    ),
-    'selected_genes_10': tf.FixedLenFeature(
-        (1121), tf.float32
-    ),
-    'selected_genes_20': tf.FixedLenFeature(
-        (2128), tf.float32
-    ),
-    'cnv_min': tf.FixedLenFeature(
-        (2128), tf.int64
-    ),
-    'cnv_max': tf.FixedLenFeature(
-        (2128), tf.int64
-    ),
-    'disrupt': tf.FixedLenFeature(
-        (2128), tf.int64
-    ),
-    'zigosity': tf.FixedLenFeature(
-        (2128), tf.int64
-    ),
-    'ic50': tf.FixedLenFeature(
-        (), tf.float32
-    ), 
-    'ic50_labels': tf.FixedLenFeature(
-        (), tf.int64
-    ), 
+    'smiles_character_tokens': tf.FixedLenFeature((158), tf.int64),
+    'smiles_atom_tokens': tf.FixedLenFeature((155), tf.int64),
+    'fingerprints_256': tf.FixedLenFeature((256), tf.int64),
+    'fingerprints_512': tf.FixedLenFeature((512), tf.int64),
+    'targets_10': tf.FixedLenFeature(((10, 1)), tf.float32),
+    'targets_20': tf.FixedLenFeature(((20, 1)), tf.float32),
+    'targets_50': tf.FixedLenFeature(((50, 1)), tf.float32),
+    'selected_genes_10': tf.FixedLenFeature((1121), tf.float32),
+    'selected_genes_20': tf.FixedLenFeature((2128), tf.float32),
+    'cnv_min': tf.FixedLenFeature((2128), tf.int64),
+    'cnv_max': tf.FixedLenFeature((2128), tf.int64),
+    'disrupt': tf.FixedLenFeature((2128), tf.int64),
+    'zigosity': tf.FixedLenFeature((2128), tf.int64),
+    'ic50': tf.FixedLenFeature((), tf.float32),
+    'ic50_labels': tf.FixedLenFeature((), tf.int64),
 }
 
-CNV_FEATURES = ['cnv_min','cnv_max','disrupt', 'zigosity']
+CNV_FEATURES = ['cnv_min', 'cnv_max', 'disrupt', 'zigosity']
 
 
-def record_parser(
-    record, params, feature_names
-):
+def record_parser(record, params, feature_names):
     """
     Process a record to create input tensors and labels.
 
@@ -95,8 +62,7 @@ def validate_feature_names(feature_names):
         A list of valid feature names.
     """
     feature_names = [
-        feature_name
-        for feature_name in feature_names
+        feature_name for feature_name in feature_names
         if feature_name in DATASETS_METADATA
     ]
     if len(feature_names) < 1:
@@ -111,8 +77,7 @@ def validate_feature_names(feature_names):
 
 
 def generate_dataset(
-    filepath, buffer_size=int(256e+6),
-    num_parallel_reads=None
+        filepath, buffer_size=int(256e+6), num_parallel_reads=None
 ):
     """
     Generate a tf.Dataset given a path.
@@ -128,12 +93,13 @@ def generate_dataset(
     else:
         filenames = [filepath]
 
-    logger.debug('Parsing examples from the following files: {}'.format(
-        filenames
-    ))
+    logger.debug(
+        'Parsing examples from the following files: {}'.format(filenames)
+    )
 
     return tf.data.TFRecordDataset(
-        filenames, buffer_size=buffer_size,
+        filenames,
+        buffer_size=buffer_size,
         num_parallel_reads=num_parallel_reads
     )
 
@@ -150,15 +116,10 @@ def get_sorted_filelist(filepath, pattern='tfrecords'):
 
     """
     return sorted([
-            filename
-            for filename in map(
-                lambda entry: os.path.join(filepath, entry),
-                os.listdir(filepath)
-            )
-            if os.path.isfile(filename) and
-            pattern in filename
-
-        ])
+        filename for filename in
+        map(lambda entry: os.path.join(filepath, entry), os.listdir(filepath))
+        if os.path.isfile(filename) and pattern in filename
+    ])
 
 
 def feature_name_to_placeholder(feature_name, params):
@@ -176,14 +137,8 @@ def feature_name_to_placeholder(feature_name, params):
     )
     placeholder_shape = [params.get('serving_batch_size', None)]
     if shape is not None:
-        placeholder_shape += (
-            [shape] if isinstance(shape, int)
-            else shape
-        )
-    return tf.placeholder(
-        feature_type,
-        placeholder_shape
-    )
+        placeholder_shape += ([shape] if isinstance(shape, int) else shape)
+    return tf.placeholder(feature_type, placeholder_shape)
 
 
 def assemble_cnv_gep_data(features, genes=[]):
@@ -206,8 +161,8 @@ def assemble_cnv_gep_data(features, genes=[]):
         tf.expand_dims(tf.cast(features['cnv_max'], tf.float32), axis=2),
         tf.expand_dims(tf.cast(features['disrupt'], tf.float32), axis=2),
         tf.expand_dims(tf.cast(features['zigosity'], tf.float32), axis=2),
-        ], axis = 2
-    )
+    ],
+                     axis=2)
     data = (
         tf.concat([tf.expand_dims(genes, axis=2), data], axis=2)
         if genes != [] else data
